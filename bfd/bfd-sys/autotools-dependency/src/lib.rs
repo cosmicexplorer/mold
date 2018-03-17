f0b1b01a87ee83ebe86e834ddf983e1459caa717
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-03-17 16:06:18 -0700

    add some diagnostics

--- bfd/bfd-sys/autotools-dependency/src/lib.rs
+++ bfd/bfd-sys/autotools-dependency/src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(slice_concat_ext)]
 #![feature(type_ascription)]
 
-extern crate pathdiff;
+extern crate flate2;
 extern crate reqwest;
 extern crate tar;
 extern crate tempdir;
@@ -11,18 +11,18 @@ use std::convert::From;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
-use std::os::unix::io::FromRawFd;
-use std::path::PathBuf;
-use std::process::{Command, ExitStatus, Stdio};
+use std::path::{Path, PathBuf};
+use std::process::{self, Command, ExitStatus, Stdio};
 use std::time;
 
-use reqwest::{IntoUrl, Response};
+use flate2::read::GzDecoder;
 use tempdir::TempDir;
 
 #[derive(Debug)]
 pub enum FetchError {
   IoError(io::Error),
   RequestError(reqwest::Error),
+  ParseError(reqwest::UrlError),
 }
 
 impl From<io::Error> for FetchError {
@@ -37,26 +37,36 @@ impl From<reqwest::Error> for FetchError {
   }
 }
 
-pub fn fetch_decompress<T: IntoUrl>(
-  url: T,
-) -> reqwest::Result<Response> {
+impl From<reqwest::UrlError> for FetchError {
+  fn from(error: reqwest::UrlError) -> Self {
+    FetchError::ParseError(error)
+  }
+}
+
+pub fn fetch_decompress(
+  url_str: &str,
+) -> Result<GzDecoder<reqwest::Response>, FetchError> {
+  eprintln!("downloading .tar.gz file from '{}'...", url_str);
   let client = reqwest::Client::builder()
     .timeout(time::Duration::new(120, 0))
     .gzip(true)
     .build()?;
-  client.get(url).send()
+  let parsed_url = reqwest::Url::parse(&url_str)?;
+  let resp = client.get(parsed_url).send()?;
+  Ok(GzDecoder::new(resp))
 }
 
-pub fn extract_into<T: Read>(stream: T, dest_dir: PathBuf) -> io::Result<()> {
+pub fn extract_into<T: Read>(stream: T, dest_dir: &Path) -> io::Result<()> {
   let mut ar = tar::Archive::new(stream);
   ar.unpack(dest_dir)
 }
 
-pub fn fetch_and_extract<T: IntoUrl>(
-  url: T,
-  dest_dir: PathBuf,
+pub fn fetch_and_extract(
+  url: &str,
+  dest_dir: &Path,
 ) -> Result<(), FetchError> {
-  let gz_stream = fetch_decompress(url)?;
+  let gz_stream = fetch_decompress(&url)?;
+  eprintln!("extracting from response stream into '{:?}'...", dest_dir);
   extract_into(gz_stream, dest_dir)?;
   Ok(())
 }
@@ -79,20 +89,21 @@ fn run_command(
   cwd: &PathBuf,
   vars: &HashMap<String, String>,
 ) -> Result<ExitStatus, BuildError> {
-  let status: ExitStatus = unsafe {
-    Command::new(exe_name_or_path)
-      .args(argv_not_first)
-      .current_dir(cwd)
-      .envs(vars)
-      .stdin(Stdio::null())
-      .stdout(Stdio::null())
-      .status()?
-  };
+  let cmd_str: String = argv_not_first.iter().fold(
+    String::from(exe_name_or_path.to_str().unwrap()),
+    |cmd, arg| format!("{} {}", cmd, arg),
+  );
+  eprintln!("running command '{}'", cmd_str);
+  let mut subproc: process::Child = Command::new(exe_name_or_path)
+    .args(argv_not_first)
+    .current_dir(cwd)
+    .envs(vars)
+    .stdin(Stdio::null())
+    .stdout(Stdio::piped())
+    .spawn()?;
+  io::copy(&mut subproc.stdout.take().unwrap(), &mut io::stderr())?;
+  let status: ExitStatus = subproc.wait()?;
   if !status.success() {
-    let cmd_str: String = argv_not_first.iter().fold(
-      String::from(exe_name_or_path.to_str().unwrap()),
-      |cmd, arg| format!("{} {}", cmd, arg),
-    );
     Err(BuildError::FailedCommand(format!(
       "command '{}' failed: {}",
       cmd_str, status,
@@ -109,15 +120,17 @@ pub fn run_configure(
   args: &Vec<String>,
   vars: &HashMap<String, String>,
 ) -> Result<ExitStatus, BuildError> {
-  let rel_path_to_source: PathBuf =
-    pathdiff::diff_paths(&source_dir, &build_dir).unwrap();
+  let abs_path_to_source: PathBuf = fs::canonicalize(&source_dir)?;
   let configure_path: PathBuf =
-    [rel_path_to_source, PathBuf::from("configure")]
+    [abs_path_to_source, PathBuf::from("configure")]
       .iter()
       .collect();
+  eprintln!("configure_path: {:?}", configure_path);
   let mut all_configure_args = args.clone();
-  eprintln!("!!!");
-  all_configure_args.insert(0, format!("--prefix={:?}", prefix_dir));
+  all_configure_args.append(&mut vec![
+    String::from("--prefix"),
+    String::from(prefix_dir.to_str().unwrap()),
+  ]);
   Ok(run_command(
     &configure_path,
     &all_configure_args,
@@ -166,39 +179,44 @@ impl From<io::Error> for BuildAutotoolsDependencyError {
   }
 }
 
-pub fn fetch_build_autotools_dep<T: IntoUrl>(
-  url: T,
-  outdir: PathBuf,
-  src_dirname: PathBuf,
+pub fn fetch_build_autotools_dep(
+  url: &str,
+  outdir: &Path,
+  src_dirname: &Path,
   configure_args: Vec<String>,
   env_vars: HashMap<String, String>,
   parallelism: u8,
 ) -> Result<PathBuf, BuildAutotoolsDependencyError> {
-  let outdir_abs = fs::canonicalize(outdir)?;
-  let dl_dir = TempDir::new("autotools-dl")?;
-  eprintln!("dl_dir.path().to_path_buf(): {:?}", dl_dir.path().to_path_buf());
-  fetch_and_extract(url, dl_dir.path().to_path_buf())?;
-  let downloaded_source_abs = fs::canonicalize(
-    [&dl_dir.path().to_path_buf(), &src_dirname]
-      .iter()
-      .collect(): PathBuf,
-  )?;
-  eprintln!("downloaded_source_abs: {:?}", downloaded_source_abs);
-  let build_dir = TempDir::new("autotools-build")?;
-  eprintln!("build_dir.path().to_path_buf(): {:?}", build_dir.path().to_path_buf());
+  let outdir_abs = fs::canonicalize(&outdir)?;
+  let dl_dir = TempDir::new("autotools-dl")?.path().to_path_buf();
+  eprintln!("dl_dir: {:?}", dl_dir);
 
+  fetch_and_extract(&url, dl_dir.as_path())?;
+  let downloaded_source_abs =
+    fs::canonicalize([dl_dir.as_path(), src_dirname].iter().collect(): PathBuf)?;
+  eprintln!("downloaded_source_abs: {:?}", downloaded_source_abs);
+
+  let build_dir = TempDir::new("autotools-build")?.path().to_path_buf();
+  eprintln!("build_dir: {:?}", build_dir);
+
+  // run configure script from source dir, in build dir, and set prefix outdir
+  eprintln!("running configure...");
   run_configure(
     &outdir_abs,
-    &build_dir.path().to_path_buf(),
+    &build_dir,
     &downloaded_source_abs,
     &configure_args,
     &env_vars,
   )?;
 
-  eprintln!("still alive!");
+  // build in build dir
+  eprintln!("running make...");
+  run_make(&build_dir, &vec![], &env_vars, parallelism)?;
 
+  // install to outdir
+  eprintln!("running make install...");
   run_make(
-    &build_dir.path().to_path_buf(),
+    &build_dir,
     &vec![String::from("install")],
     &env_vars,
     parallelism,
--- bfd/bfd-sys/build.rs
+++ bfd/bfd-sys/build.rs
@@ -2,22 +2,23 @@ extern crate autotools_dependency;
 
 use std::collections::HashMap;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 static BINUTILS_URL: &'static str =
   "https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz";
 
 fn main() {
-  let bfd_binutils_dirname = PathBuf::from("bfd-binutils");
-  let binutils_src_dirname = PathBuf::from("binutils-2.30");
+  let bfd_binutils_dirname = Path::new("bfd-binutils");
+  let binutils_src_dirname = Path::new("binutils-2.30");
 
   fs::DirBuilder::new()
     .recursive(true)
-    .create(bfd_binutils_dirname.clone())
+    .create(bfd_binutils_dirname)
     .unwrap();
+
   let bfd_binutils_dir = autotools_dependency::fetch_build_autotools_dep(
     BINUTILS_URL,
-    bfd_binutils_dirname.clone(),
+    bfd_binutils_dirname,
     binutils_src_dirname,
     vec![],
     HashMap::new(),
@@ -26,13 +27,12 @@ fn main() {
 
   eprintln!("bfd_binutils_dir: {:?}", bfd_binutils_dir);
 
-  let bfd_inc_dir: PathBuf =
-    [bfd_binutils_dir.clone(), PathBuf::from("include")]
-      .iter()
-      .collect();
+  let bfd_inc_dir: PathBuf = [bfd_binutils_dir.as_path(), Path::new("include")]
+    .iter()
+    .collect();
   println!("cargo:include={:?}", bfd_inc_dir);
 
-  let bfd_lib_dir: PathBuf = [bfd_binutils_dir.clone(), PathBuf::from("lib")]
+  let bfd_lib_dir: PathBuf = [bfd_binutils_dir.as_path(), Path::new("lib")]
     .iter()
     .collect();
   println!("cargo:rustc-link-search=native={:?}", bfd_lib_dir);
