0acf40fee0a387d19384239a1f168e9a7165b318
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-03-17 22:59:00 -0700

    make bfd-sys a real *-sys package

--- bfd/bfd-sys/build.rs
+++ bfd/bfd-sys/build.rs
@@ -1,4 +1,5 @@
 extern crate autotools_dependency;
+extern crate bindgen;
 
 use std::collections::HashMap;
 use std::fs;
@@ -17,46 +18,69 @@ fn main() {
   let bfd_binutils_dirname = Path::new("bfd-binutils");
   let binutils_src_dirname = Path::new("binutils-2.30");
 
-  fs::DirBuilder::new()
-    .recursive(true)
-    .create(bfd_binutils_dirname)
-    .unwrap();
+  let bfd_lib_loc: PathBuf = [bfd_binutils_dirname, Path::new("lib/libbfd.a")]
+    .iter()
+    .collect();
+  let bfd_binutils_dir = if fs::metadata(bfd_lib_loc).unwrap().is_file() {
+    fs::canonicalize(bfd_binutils_dirname).unwrap()
+  } else {
+    fs::DirBuilder::new()
+      .recursive(true)
+      .create(bfd_binutils_dirname)
+      .unwrap();
 
-  let mut config_env: HashMap<String, String> = HashMap::new();
-  config_env.insert("PATH".to_string(), "/bin:/usr/bin".to_string());
+    let mut config_env: HashMap<String, String> = HashMap::new();
+    config_env.insert("PATH".to_string(), "/bin:/usr/bin".to_string());
 
-  let config_args = to_string_vec(&[
-    "CFLAGS=-arch x86_64",
-    "LDFLAGS=-arch x86_64",
-    "CC=clang",
-    "CXX=clang++",
-    "--disable-werror",
-    "--enable-64-bit-bfd",
-    "--build=x86_64-apple-darwin",
-    "--host=x86_64-apple-darwin",
-    "--target=x86_64-apple-darwin",
-  ]);
-  let bfd_binutils_dir = autotools_dependency::fetch_build_autotools_dep(
-    BINUTILS_URL,
-    bfd_binutils_dirname,
-    binutils_src_dirname,
-    config_args,
-    config_env,
-    time::Duration::new(300, 0),
-    1,
-  ).unwrap();
+    let config_args = to_string_vec(&[
+      "CFLAGS=-arch x86_64",
+      "LDFLAGS=-arch x86_64",
+      "CC=clang",
+      "CXX=clang++",
+      "--disable-werror",
+      "--enable-64-bit-bfd",
+      "--build=x86_64-apple-darwin",
+      "--host=x86_64-apple-darwin",
+      "--target=x86_64-apple-darwin",
+    ]);
 
-  eprintln!("bfd_binutils_dir: {:?}", bfd_binutils_dir);
+    let install_dir = autotools_dependency::fetch_build_autotools_dep(
+      BINUTILS_URL,
+      bfd_binutils_dirname,
+      binutils_src_dirname,
+      config_args,
+      config_env,
+      time::Duration::new(300, 0),
+      1,
+    ).unwrap();
+
+    eprintln!("install_dir: {:?}", install_dir);
+
+    install_dir
+  };
 
   let bfd_inc_dir: PathBuf = [bfd_binutils_dir.as_path(), Path::new("include")]
     .iter()
     .collect();
-  println!("cargo:include={}", bfd_inc_dir.to_string_lossy());
+  // bfd.h requires PACKAGE or PACKAGE_VERSION to be defined, or it errors out
+  // during preprocessing. This seems like a failure
+  let bfd_bindings = bindgen::builder()
+    .clang_arg("-DPACKAGE")
+    .clang_arg(format!("-I{}", bfd_inc_dir.to_str().unwrap()))
+    .header("include/bfd-headers.h")
+    .generate()
+    .unwrap();
+  bfd_bindings
+    .write_to_file("include/bfd-bindings.rs")
+    .unwrap();
 
   let bfd_lib_dir: PathBuf = [bfd_binutils_dir.as_path(), Path::new("lib")]
     .iter()
     .collect();
-  println!("cargo:rustc-link-search=native={}", bfd_lib_dir.to_string_lossy());
+  println!(
+    "cargo:rustc-link-search=native={}",
+    bfd_lib_dir.to_str().unwrap()
+  );
 
   println!("cargo:rustc-link-lib=static=bfd");
 }
--- bfd/bfd-sys/src/lib.rs
+++ bfd/bfd-sys/src/lib.rs
@@ -1,3 +1,9 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!("../include/bfd-bindings.rs");
+
 #[cfg(test)]
 mod tests {
     #[test]
