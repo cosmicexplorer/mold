67265d441fd7c03ccf70008da9b75ff311a97036
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-03-18 11:07:43 -0700

    improve build script for bfd-sys just a little bit

--- bfd-sys/build.rs
+++ bfd-sys/build.rs
@@ -18,6 +18,43 @@ fn to_string_vec(args: &[&str]) -> Vec<String> {
   args.iter().map(|s| s.to_string()).collect()
 }
 
+fn build_binutils(src_dir: &Path, build_dir: &Path) {
+  fs::DirBuilder::new()
+    .recursive(true)
+    .create(build_dir)
+    .unwrap();
+
+  let mut config_env: HashMap<String, String> = HashMap::new();
+  config_env.insert("PATH".to_string(), "/bin:/usr/bin".to_string());
+
+  let config_args = to_string_vec(&[
+    "CFLAGS=-arch x86_64",
+    "LDFLAGS=-arch x86_64",
+    "CC=clang",
+    "CXX=clang++",
+    "--disable-werror",
+    "--enable-64-bit-bfd",
+    "--build=x86_64-apple-darwin",
+    "--host=x86_64-apple-darwin",
+    "--target=x86_64-apple-darwin",
+  ]);
+
+  // let tmp_build_dir = TempDir::new("autotools-build").unwrap();
+  // let build_dir_abs: PathBuf = fs::canonicalize(tmp_build_dir.path()).unwrap();
+
+  let FetchedAutotoolsDep {
+    build_dir: complete_build_dir,
+  } = autotools_dependency::build_local_autotools_dep(
+    // BINUTILS_URL,
+    src_dir,
+    build_dir,
+    config_args,
+    config_env,
+    // time::Duration::new(300, 0),
+    4,
+  ).unwrap();
+}
+
 fn file_exists(path: &Path) -> bool {
   if let Ok(metadata) = fs::metadata(path) {
     metadata.is_file()
@@ -28,7 +65,10 @@ fn file_exists(path: &Path) -> bool {
 
 fn add_static_build_lib(build_dir: &Path, subdir: &str, lib_name: &str) {
   let lib_dir: PathBuf = [build_dir, Path::new(subdir)].iter().collect();
-  println!("cargo:rustc-link-search=native={}", lib_dir.to_str().unwrap());
+  println!(
+    "cargo:rustc-link-search=native={}",
+    lib_dir.to_str().unwrap()
+  );
   println!("cargo:rustc-link-lib=static={}", lib_name);
 }
 
@@ -48,48 +88,16 @@ fn main() {
     );
   }
 
-  // fs::DirBuilder::new()
-  //   .recursive(true)
-  //   .create(bfd_binutils_dirname)
-  //   .unwrap();
-
-  let mut config_env: HashMap<String, String> = HashMap::new();
-  config_env.insert("PATH".to_string(), "/bin:/usr/bin".to_string());
-
-  let config_args = to_string_vec(&[
-    "CFLAGS=-arch x86_64",
-    "LDFLAGS=-arch x86_64",
-    "CC=clang",
-    "CXX=clang++",
-    "--disable-werror",
-    "--enable-64-bit-bfd",
-    "--build=x86_64-apple-darwin",
-    "--host=x86_64-apple-darwin",
-    "--target=x86_64-apple-darwin",
-  ]);
-
   let src_dir = Path::new("/Users/dmcclanahan/tools/binutils-2.30");
   let build_dir = Path::new("/Users/dmcclanahan/tools/binutils-osx-build");
 
-  // let tmp_build_dir = TempDir::new("autotools-build").unwrap();
-  // let build_dir_abs: PathBuf = fs::canonicalize(tmp_build_dir.path()).unwrap();
+  let bfd_archive_cached: PathBuf =
+    [build_dir, Path::new("bfd/libbfd.a")].iter().collect();
+  if !file_exists(bfd_archive_cached.as_path()) {
+    build_binutils(src_dir, build_dir);
+  }
 
-  let FetchedAutotoolsDep {
-    build_dir: complete_build_dir,
-  } = autotools_dependency::build_local_autotools_dep(
-    // BINUTILS_URL,
-    src_dir,
-    build_dir,
-    config_args,
-    config_env,
-    // time::Duration::new(300, 0),
-    4,
-  ).unwrap();
-
-  let bfd_inc_dir: PathBuf =
-    [complete_build_dir.as_path(), Path::new("bfd")]
-      .iter()
-      .collect();
+  let bfd_inc_dir: PathBuf = [build_dir, Path::new("bfd")].iter().collect();
   // bfd.h requires PACKAGE or PACKAGE_VERSION to be defined, or it errors out
   // during preprocessing. This seems like a failure
   let bfd_bindings = bindgen::builder()
@@ -104,16 +112,11 @@ fn main() {
     .write_to_file("include/bfd-bindings.rs")
     .unwrap();
 
-  let build_path = complete_build_dir.as_path();
-  add_static_build_lib(build_path, "bfd", "bfd");
-
-  add_static_build_lib(build_path, "zlib", "z");
-
-  add_static_build_lib(build_path, "intl", "intl");
-
-  add_static_build_lib(build_path, "libiberty", "iberty");
-
-  add_static_build_lib(build_path, "opcodes", "opcodes");
+  add_static_build_lib(build_dir, "bfd", "bfd");
+  add_static_build_lib(build_dir, "zlib", "z");
+  add_static_build_lib(build_dir, "intl", "intl");
+  add_static_build_lib(build_dir, "libiberty", "iberty");
+  add_static_build_lib(build_dir, "opcodes", "opcodes");
 
   println!("cargo:rustc-link-lib=dylib={}", "iconv");
 }
